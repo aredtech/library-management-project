20084bdd80984ceb5b026490ad8da0560656ceca
aredtech <raju.subharth.pay07@gmail.com>
2021-12-06 14:25:04 +0530

    Add readme.md

--- database_creation.py
+++ database_creation.py
@@ -1,14 +1,21 @@
 import mysql.connector
-# python -m pip install mysql-connector-python
+
+# For Windows
+# pip install mysql-connector-python
+# pip install mysql-connector
+
+# For Mac
+# pip3 install mysql-connector
+# pip3 install mysql-connector-python
 
 db = mysql.connector.connect(
     host="localhost",
-    user="root",
-    passwd="root",
-    database="libraryproject"
+    user="root", # Change if necessary
+    passwd="root", # Change if necessary
+    # database="libraryproject" # Uncomment After Database creation
 )
 
-mycursor = db.cursor()
+# mycursor = db.cursor()
 # mycursor.execute("CREATE DATABASE libraryproject")
 
 # Check If Database is Created or not
@@ -18,7 +25,8 @@ mycursor = db.cursor()
 
 
 
-# Create Table for books
+# CREATE TABKE FOR BOOK DETAILS
+
 # mycursor.execute("CREATE TABLE books (book_id smallint PRIMARY KEY AUTO_INCREMENT, book_name VARCHAR(250), author_name VARCHAR(50), price smallint, stock smallint, edition smallint)")
 
 # Check if table is created or not :
@@ -26,8 +34,8 @@ mycursor = db.cursor()
 # for x in mycursor:
 #     print(x)
 
-# Table for members
+# CREATES TABLE FOR MEMBER DETAILS
 # mycursor.execute("CREATE TABLE members (member_id smallint PRIMARY KEY AUTO_INCREMENT, member_name VARCHAR(50), member_address VARCHAR(250), member_phone VARCHAR(14))")
 
-# Table for book_issue
-mycursor.execute("CREATE TABLE issues (s_no smallint PRIMARY KEY AUTO_INCREMENT, member_id smallint, FOREIGN KEY (member_id) REFERENCES members(member_id), book_id smallint, FOREIGN KEY (book_id) REFERENCES books(book_id), date_issue DATE, date_return DATE )")
+# CREATES TABLE FOR STORING BOOK ISSUING Details
+# mycursor.execute("CREATE TABLE issues (s_no smallint PRIMARY KEY AUTO_INCREMENT, member_id smallint, FOREIGN KEY (member_id) REFERENCES members(member_id), book_id smallint, FOREIGN KEY (book_id) REFERENCES books(book_id), date_issue DATE, date_return DATE )")
---
+++ readme.md
@@ -0,0 +1,15 @@
+## Library Management GUI 
+
+A tkinter (Python) based GUI software made for a project work given by ducat. Made the software in a week.
+
+
+### Requirements
+
+* Python 3.XX
+* MySQL
+* MySQL Connector for Python
+
+
+### How to Setup the Database for the Software?
+#### Read the readme.md
+
